4fbac93b94031a3c70f9f1b511947787f7c1650e
Abhishek Tiwari <tiwari.abhishektiwari23@gmail.com>
2023-06-16 20:40:56 +0530

    added get user by email

--- backend/apis/routes/route_users.py
+++ backend/apis/routes/route_users.py
@@ -1,10 +1,10 @@
-from database.repository.users import create_new_user
+from database.repository.users import create_new_user, get_user_by_email
 from database.sessions import get_db
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 
 from sqlalchemy.orm import Session
-from schemas.users import UserCreate
+from schemas.users import UserCreate, ShowUser
 
 user_router = APIRouter()
 
@@ -13,4 +13,12 @@ user_router = APIRouter()
 @user_router.post("/",response_model=UserCreate)
 def create_user(user: UserCreate, db: Session = Depends(get_db)):
     user = create_new_user(user, db)
+    return user
+
+# Route to get User By Email - Working
+@user_router.get("/{email}", response_model=ShowUser)
+def get_user(email: str, db: Session = Depends(get_db)):
+    user = get_user_by_email(email, db)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"User with email {email} not found")
     return user
--- backend/database/repository/users.py
+++ backend/database/repository/users.py
@@ -29,3 +29,8 @@ def create_new_user(user: UserCreate, db: Session):
     db.commit()
     db.refresh(new_user)
     return new_user
+
+# Get a user by email - Working
+def get_user_by_email(email: str, db: Session):
+    user = db.query(User).filter(User.email == email).first()
+    return user
