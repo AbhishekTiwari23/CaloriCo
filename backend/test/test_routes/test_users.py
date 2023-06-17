e906f8ecaa3a5fd45df1702b49481abf0995377b
Abhishek Tiwari <tiwari.abhishektiwari23@gmail.com>
2023-06-18 02:52:00 +0530

    test get user by username with incorrect credentials

--- backend/test/test_routes/test_users.py
+++ backend/test/test_routes/test_users.py
@@ -22,3 +22,27 @@ def test_get_user_by_username(client):
     assert response.status_code == 200
     assert response.json()["username"] == "testuser"
     assert response.json()["email"] == "testuser@nofoobar.com"
+
+# test to get user by username with invalid token
+def test_get_user_by_username(client):
+
+    # Arrange
+    user_data = {
+        "first_name": "John",
+        "last_name": "Doe",
+        "username": "testuser",
+        "email": "testuser@nofoobar.com",
+        "password": "testing",
+        "join_date": "2021-01-01",
+        "role": "user",
+        "expected_calories": 2000
+
+    }
+    crate_user_response = client.post("/auth/SighUp", json=user_data)
+
+    # Act
+    response = client.get("/users/username/testuser?auth_token=" +"" + "invalid")
+
+    assert response.status_code == 401
+    assert response.json()["detail"] == "Invalid authentication credentials"
+
