702f079a1535560c42be9e6b8cbb24b080ef82b0
Abhishek Tiwari <tiwari.abhishektiwari23@gmail.com>
2023-06-18 03:02:35 +0530

    get user by email neg test

--- backend/test/test_routes/test_users.py
+++ backend/test/test_routes/test_users.py
@@ -70,3 +70,27 @@ def test_get_user_by_email(client):
     assert response.status_code == 200
     assert response.json()["username"] == "testuser"
     assert response.json()["email"] == "testuser@nofoobar.com"
+
+# test to get user by email
+def test_get_user_by_email_incorrect(client):
+
+    # Arrange
+    user_data = {
+        "first_name": "John",
+        "last_name": "Doe",
+        "username": "testuser",
+        "email": "testuser@nofoobar.com",
+        "password": "testing",
+        "join_date": "2021-01-01",
+        "role": "user",
+        "expected_calories": 2000
+
+    }
+    crate_user_response = client.post("/auth/SighUp", json=user_data)
+    auth_user_response = client.post("/auth/token", data={"username": "testuser", "password": "testing"})
+
+    # Act
+    response = client.get("/usersEmail/testuser@nobar.com?auth_token=" + auth_user_response.json()["access_token"])
+
+    assert response.status_code == 404
+    assert response.json()["detail"] == "User with email testuser@nobar.com not found"
