286d90dcb5b72e3a03e2aaf6a18a9253d3fd3dd3
Abhishek Tiwari <tiwari.abhishektiwari23@gmail.com>
2023-06-18 23:57:33 +0530

    test for null calories

--- backend/test/test_routes/test_food.py
+++ backend/test/test_routes/test_food.py
@@ -1,3 +1,5 @@
+from core.calories import get_calories
+import json
 # test to create a new food
 def test_create_new_food(client):
     # Arrange
@@ -41,8 +43,8 @@ def test_create_new_food(client):
     assert response.json()["quantity"] == 1
     assert response.json()["calories"] == 95
 
-# test to get all food for a user
-def test_get_all_food(client):
+# test to create a new food
+def test_create_new_food_not_calory(client):
     # Arrange
     user_data = {
         "first_name": "John",
@@ -51,10 +53,40 @@ def test_get_all_food(client):
         "email": "testuser@nofoobar.com",
         "password": "testing",
         "join_date": "2021-01-01",
-        "role": "user",
+        "role": "admin",
         "expected_calories": 2000
-
     }
-    crate_user_response = client.post("/auth/SighUp", json=user_data)
-    assert crate_user_response.status_code == 200
+
+    create_user_response = client.post("/auth/SighUp", json=user_data)
+    assert create_user_response.status_code == 200
     auth_user_response = client.post("/auth/token", data={"username": "testuser", "password": "testing"})
+    assert auth_user_response.status_code == 200
+
+    food_data = {
+        "name": "apple",
+        "date": "2023-06-18",
+        "time": "07:55:13",
+        "quantity": 1,
+        "calories": ""
+    }
+
+    # Act
+    access_token = auth_user_response.json()["access_token"]
+    response = client.post(
+        f"/food/testuser/new_food?auth_token={access_token}",
+        json=food_data,
+        headers={"Authorization": f"Bearer {access_token}"}
+    )
+    add_calories = response.json()["calories"]
+    if not response.json()["calories"]:
+            api_response = get_calories(food_data["name"], food_data["quantity"])
+            data = json.loads(api_response)
+            add_calories = data['items'][0]['calories']
+
+    # Assert
+    assert response.status_code == 200
+    assert response.json()["name"] == "apple"
+    assert response.json()["date"] == "2023-06-18"
+    assert response.json()["time"] == "1900-01-01 07:55:13"
+    assert response.json()["quantity"] == 1
+    assert response.json()["calories"] == add_calories
