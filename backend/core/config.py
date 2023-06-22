2ee9eef43866d014d3e5f0ec6c2901e23e490e50
Abhishek Tiwari <tiwari.abhishektiwari23@gmail.com>
2023-06-22 15:53:25 +0530

    removed git cache

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,6 +1,6 @@
 class Settings:
-    PROJECT_NAME: str = "Octernship-Tasks"
-    PROJECT_VERSION: str = "0.1.0"
+    PROJECT_NAME: str = "CaloriCo"
+    PROJECT_VERSION: str = "1.0.0"
 
     # Authentication
     SECRET_KEY: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJvY3Rlcm5zaGlwVGFzayIsIm5hbWUiOiJBYmhpc2hlayBUaXdhcmkiLCJpYXQiOjIwMjN9.2YAsRhnuehDTtz2Hqw-njRMSg2xaf0ssLkACqlVyNWw"
